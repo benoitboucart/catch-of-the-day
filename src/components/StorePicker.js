4e857199d291a8b5f0fe0f4b645b0ebf1cf3678c
Benoît Boucart <benoit@webbb.be>
2018-06-18 15:51:45 +0200

    04 - Writing HTML with JSX

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>
+    return (
+      <form className="store-selector">
+        { /* This is a comment in JSX */ }
+        <h2>Please enter a store</h2>
+        <input type="text" required placeholder="Store name"/>
+        <button type="submit">Visit store</button>
+      </form>
+    )
   }
 }
 
