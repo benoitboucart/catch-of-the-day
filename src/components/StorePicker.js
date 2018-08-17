5e6d6d73c24d979c6b1f973a888fc1fc57c37306
Benoît Boucart <benoit@webbb.be>
2018-08-17 10:04:54 +0200

    12 - All About React Router

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { getFunName } from '../helpers';
-
+//import { getFunName } from '../helpers';
+import * as helpers from '../helpers';
+import { PropTypes  } from 'prop-types';
 class StorePicker extends React.Component {
 
-  constructor() {
-    super();
-    // Make sure to make "this" available in goToStore function (or use goToStore as arrow function)
-    this.goToStore = this.goToStore.bind(this);
+  static propTypes = {
+    history: PropTypes.object.isRequired
   }
 
-  goToStore(event) {
-    event.preventDefault();
-    // Grab text from box
-    const text = this.storeInput.value;
-
-    // Transition from / to /store/id
-  }
-
-  // goToStore = (e) => {
-  //   e.preventDefault();
-  //   console.log(`You changed URL`);
-
-  //   // Grab text from box
-  //   console.log(this);
-  //   console.log(this.storeInput);
-  //   // Transition from / to /store/id
+  // constructor() {
+  //   super();
+  //   // Make sure to make "this" available in goToStore function (or use goToStore as arrow function)
+  //   this.goToStore = this.goToStore.bind(this);
   // }
+
+  goToStore = e => {
+    // If you write this function as goToStore(e) {...
+    // then in order to use 'this' inside of it,
+    // you need to bind this function to the component using the constructor function above
+    // 1. Stop the form from submitting
+    e.preventDefault();
+    // 2. Get the text form that input
+    const storeName = this.storeInput.value;
+    // 3. Change the page to /store/whatever-they-entered
+    // Can use history.push because it's a prop passed to StorePicker from Router
+    this.props.history.push(`/store/${storeName}`); // React Router renders the right component for this
+  }
 
   render() {
     return (
@@ -33,7 +33,7 @@ class StorePicker extends React.Component {
         { /* This is a comment in JSX */ }
         <h2>Please enter a store</h2>
         <input type="text" required placeholder="Store name"
-          defaultValue={getFunName()}
+          defaultValue={helpers.getFunName()}
           ref={(input)=>{ this.storeInput = input }}
         />
         <button type="submit">Visit store</button>
